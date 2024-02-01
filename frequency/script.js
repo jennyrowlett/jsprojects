818dc089537c2a320680ff99b3aad079bbcc60ff
Jenny Rowlett <jrowlett@oberlin.edu>
2024-02-01 14:40:21 -0500

    axes, interactive chart

--- frequency/script.js
+++ frequency/script.js
@@ -42,26 +42,60 @@ function update(data) {
 
   let leftAxis = d3.axisLeft(yScale);
 
-  topContainer.call(topAxis);
+  topContainer.transition().call(topAxis);
 
-  leftContainer.call(leftAxis);
+  leftContainer.transition().call(leftAxis);
+
+  function getClass(char) {
+    if (/^[a-z]$/.test(char)) {
+      return "lower";
+    } else if (/^[A-Z]$/.test(char)) {
+      return "upper";
+    } else if (/^[0-9]$/.test(char)) {
+      return "number";
+    } else {
+      return "other";
+    }
+  }
 
   svg
     .selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("width", (d, i) => xScale(d.count) - xScale(0))
-    .attr("height", yScale.bandwidth())
-    .attr("x", xScale(0))
-    .attr("y", (d, i) => yScale(d.char));
+    .data(data, (d) => d.char)
+    .join(
+      (enter) =>
+        enter
+          .append("rect")
+          .attr("x", xScale(0))
+          .attr("y", (d, i) => yScale(d.char))
+          .attr("class", (d) => getClass(d.char))
+          .transition()
+          .attr("width", (d) => xScale(d.count) - xScale(0))
+          .attr("height", yScale.bandwidth()),
+      (update) =>
+        update
+          .transition()
+          .attr("width", (d) => xScale(d.count) - xScale(0))
+          .attr("height", yScale.bandwidth())
+          .attr("y", (d, i) => yScale(d.char)),
+      (exit) => exit.transition().attr("width", 0).attr("height", 0).remove()
+    );
+}
+
+function standardizeSpace(char) {
+  if (char.trim() == " ") {
+    return "<space>";
+  } else {
+    return char;
+  }
 }
 
 d3.select("textarea").on("input", (e) => {
   let frequencies = {};
 
   e.target.value.split("").forEach((char) => {
-    let currentCount = frequencies[char] || 0;
-    frequencies[char] = currentCount + 1;
+    let standardized = standardizeSpace(char);
+    let currentCount = frequencies[standardized] || 0;
+    frequencies[standardized] = currentCount + 1;
   });
   let data = Object.entries(frequencies).map((pair) => {
     //use Object.entries to convert the frequencies object into an array of two-element arrays
